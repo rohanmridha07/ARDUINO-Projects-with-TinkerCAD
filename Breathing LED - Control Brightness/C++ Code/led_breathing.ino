60668e2668ddb95f5fd49d5ae687000b32035fa6
Rohan Mridha <rohanmridha7x@gmail.com>
2025-06-20 23:08:09 +0600

    led_breathing.ino

---
+++ Breathing LED - Control Brightness/C++ Code/led_breathing.ino
@@ -0,0 +1,23 @@
+// C++ code
+// Define the PWM pin connected to the LED
+int LEDpin = 11;
+void setup()
+{
+  // Set the pin as an output
+  pinMode(LEDpin, OUTPUT);
+}
+void loop() 
+{
+  // Fade in (brightness from 0 to 255)
+  for (int a = 0; a <= 255; a++) {
+    analogWrite(LEDpin, a);
+    delay(20); // Wait for 20ms to see the dimming effect
+  }
+  // Fade out (brightness from 255 to 0)
+  for (int a = 255; a >= 0; a--) {
+    analogWrite(LEDpin, a);
+    delay(20); // Wait for 20ms to see the dimming effect
+  }
+  // A short pause at the end of the cycle
+  delay(200);
+}

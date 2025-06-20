e6aabd144940bc9b12f585c2ce2da428b5ce1946
Rohan Mridha <rohanmridha7x@gmail.com>
2025-06-20 20:53:57 +0600

    blink_an_led.ino

---
+++ Blink an LED/C++ Code/blink_an_led.ino
@@ -0,0 +1,12 @@
+int LED = 13;
+void setup()
+{
+ pinMode(LED, OUTPUT);
+}
+void loop()
+{
+digitalWrite(LED, HIGH); // Turn the LED on (HIGH is the voltage level)
+delay(500); // Wait for 500 milliseconds
+digitalWrite(LED, LOW); // Turn the LED off by making the voltage LOW
+delay(500); // Wait for 500 milliseconds
+}

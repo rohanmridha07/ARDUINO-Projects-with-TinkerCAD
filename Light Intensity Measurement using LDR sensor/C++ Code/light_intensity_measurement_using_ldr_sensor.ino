81a53e0d9c35909adbe9f5dfab4aa4b0143542e5
Rohan Mridha <rohanmridha7x@gmail.com>
2025-06-23 23:14:15 +0600

    light_intensity_measurement_using_ldr_sensor.ino

---
+++ Light Intensity Measurement using LDR sensor/C++ Code/light_intensity_measurement_using_ldr_sensor.ino
@@ -0,0 +1,19 @@
+
+// C++ code
+// Author: Rohan Mridha
+// EEE-23, KUET
+int sensorValue = 0 ;
+void setup()
+{
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  sensorValue = analogRead(A0);
+  Serial.println (sensorValue);
+  analogWrite (9, map (sensorValue, 0, 1023, 0, 255));
+  delay(100); 
+}
